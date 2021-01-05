9e4af76e5016a9e5f1f4e93dac5d807d23ed5a24
Lucas Dahl <LucasDahl425@yahoo.com>
2021-01-05 10:55:04 -0800

    Added notes about chars and strings.

---
+++ CPP132/Notes/charString.cpp
@@ -0,0 +1,27 @@
+//
+//  charString.cpp
+//  CPP132
+//
+//  Created by Lucas Dahl on 1/5/21.
+//
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    
+    char x = 'a';
+    char z[12] = "Hello World"; // secret char '\0' null character to signify the end of the string
+    
+    x = toupper(x);
+    
+    //cin.getline(x,y) // This will cut some of the data based off a size(y) and x would be user input.
+    
+    cout << x;
+    
+    cout << z << endl;
+   // cout << z.length() << endl;
+    
+}
