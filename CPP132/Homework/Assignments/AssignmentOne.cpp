71e7a0ec4813ebea9c8ea0e1b0ddea429ca82e06
Lucas Dahl <LucasDahl425@yahoo.com>
2021-01-08 20:24:28 -0800

    Added notes about structs.

---
+++ CPP132/Homework/Assignments/AssignmentOne.cpp
@@ -0,0 +1,32 @@
+//
+//  AssignmentOne.cpp
+//  CPP132
+//
+//  Created by Lucas Dahl on 1/7/21.
+//
+
+//#include <iostream>
+//#include <string>
+//
+//using namespace std;
+//
+//int main() {
+//
+//    // allocate space for a array to create an array of a unquie size(is needed)
+//    // new is creating something on the heap(doing new we will have to delete the property).
+//    double list[100];
+//    int size;
+//    size = 431;
+//    string *myList;
+//    myList = new string[size];
+//    myList[0] = "Hello";
+//    myList[430] = "end";
+//    cout << myList[0] << " " << myList[430];
+//    delete [] myList;
+//    
+//}
+
+// dont do for sting length - use while loop and counter
+//int findStrLength(char x[]) {
+//    return strlen(x);
+//}
---
+++ CPP132/Notes/StructData.cpp
@@ -0,0 +1,97 @@
+//
+////  StructData.cpp
+////  CPP132
+////
+////  Created by Lucas Dahl on 1/8/21.
+////
+//
+//#include <iostream>
+//#include <string>
+//
+//using namespace std;
+//
+//struct Time { // doesnt have to be caps, but probaly should be  -- Must make structs before functions, no prototype
+//    // Structs can have their own file.
+//
+//    int hours;
+//    int minutes;
+//    int seconds;
+//    
+//    bool am;
+//    
+//}; // Structs need ; at the end
+//
+//// Prototype
+//bool compareTime(Time a, Time b);
+//void addSecond(Time a);
+//void printTime(Time a);
+//
+//int main() {
+//    
+//    Time now;
+//    Time dinner;
+//    Time bedTime;
+//    
+//    now.hours = 10;
+//    now.minutes = 27;
+//    now.seconds = 45;
+//    now.am = true;
+//    
+//    dinner.hours = 6;
+//    dinner.minutes = 30;
+//    dinner.seconds = 0;
+//    dinner.am = false;
+//    
+//    bedTime.hours = 10;
+//    bedTime.minutes = 15;
+//    bedTime.seconds = 59;
+//    bedTime.am = false;
+//    
+//    cout << "You got grounded";
+//    bedTime.hours = bedTime.hours - 2;
+//    
+//    if(compareTime(now, dinner)) {
+//        cout << "Lets eat!";
+//    }
+//    
+//    
+//    while (true) {
+//        printTime(now);
+//        addSecond(now);
+//        cout << endl;
+//    }
+//    
+//}
+//
+//
+//bool compareTime(Time a, Time b) {
+//    if(a.hours != b.hours) return false;
+//    if(a.minutes != b.minutes) return false;
+//    if(a.seconds != b.seconds) return false;
+//    if(a.am != b.am) return false;
+//    return true;
+//}
+//
+//void addSecond(Time &a) {
+//    a.seconds++;
+//    if(a.seconds == 60) {
+//        a.minutes++;
+//        a.seconds = 0;
+//    }
+//    
+//    if(a.minutes == 60) {
+//        a.hours++;
+//        a.minutes = 0;
+//    }
+//    
+//    if(a.hours == 13) {
+//        a.hours = 1;
+//        a.am != a.am;
+//    }
+//}
+//
+//void printTime(Time a) {
+//    cout << a.hours << ":" << a.minutes << ":" << a.seconds << " ";
+//    if(a.am) cout << "AM";
+//    else cout << "PM";
+//}
