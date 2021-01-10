ea77ccd596c4ceda2b6d1ec9ffc8538997279607
Lucas Dahl <LucasDahl425@yahoo.com>
2021-01-09 21:17:18 -0800

    Did the bulk of the first assignment.

--- CPP132/Homework/Assignments/AssignmentOne.cpp
+++ CPP132/Homework/Assignments/AssignmentOne.cpp
@@ -5,28 +5,110 @@
 //  Created by Lucas Dahl on 1/7/21.
 //
 
-//#include <iostream>
-//#include <string>
-//
-//using namespace std;
-//
-//int main() {
-//
-//    // allocate space for a array to create an array of a unquie size(is needed)
-//    // new is creating something on the heap(doing new we will have to delete the property).
-//    double list[100];
-//    int size;
-//    size = 431;
-//    string *myList;
-//    myList = new string[size];
-//    myList[0] = "Hello";
-//    myList[430] = "end";
-//    cout << myList[0] << " " << myList[430];
-//    delete [] myList;
-//    
-//}
+#include <iostream>
+#include <string>
+#include <fstream>
 
-// dont do for sting length - use while loop and counter
-//int findStrLength(char x[]) {
-//    return strlen(x);
-//}
+using namespace std;
+
+int main() {
+    
+    // Properties
+    int userNum, totalWords = 0, index = 0;
+    ifstream inputFile;
+    ofstream outputFile("WordFile2.txt"); // The new Word file.
+    string currentLine, longestWord, *wordList, fileName = "WordList.txt";
+    bool flag = true;
+
+    cout << "Formal Name: Lucas Dahl" << endl; // Formal name.
+    cout << "Preferred Name: Lucas Dahl" << endl; // Preferred name.
+    
+    do {
+        
+        cout << "What size do you want? "; // Ask for a number
+        cin >> userNum; // assign the number.
+        
+        if(userNum < 1 || userNum > 30) {
+            cout << "Number must be bewtween 1 and 30." << endl;
+        } else {
+            flag = false;
+        }
+        
+    } while(flag);
+    
+    inputFile.open(fileName); // open the file.
+    
+    if(inputFile) {
+        
+        while(getline(inputFile, currentLine)) {
+            
+            if(currentLine.length() == userNum) {
+                totalWords++;
+            }
+            
+        }
+        
+    } else {
+        
+        cout << "Could not open the file.." << endl;
+        
+    }
+    
+    inputFile.close(); // Close the file.
+    
+    wordList = new string[totalWords]; // Create an array.
+    inputFile.open(fileName); // reopen the file.
+    
+    if(inputFile) {
+        
+        while(getline(inputFile, currentLine)) {
+            
+            if(currentLine.length() == userNum) {
+                wordList[index] = currentLine;
+                index++;
+            }
+            
+        }
+        
+    } else {
+        
+        cout << "Could not open the file.." << endl;
+        
+    }
+    
+    
+    for(int i = 1; i < totalWords; i++) {
+        cout << wordList[i - 1] << endl; // Print each word to the screen.
+        outputFile << wordList[totalWords - i] << endl; // Write to the file backwards.
+    }
+    
+    // Close both files.
+    inputFile.close();
+    outputFile.close();
+    
+    inputFile.open(fileName); // reopen the file.
+    
+    // Find the longest word.
+    if(inputFile) {
+        
+        while(getline(inputFile, currentLine)) {
+            
+            if(currentLine.length() > longestWord.length()) {
+                longestWord = currentLine;
+            }
+            
+        }
+        
+    } else {
+        
+        cout << "Could not open the file.." << endl;
+        
+    }
+    
+    cout << "The longest word is: " << longestWord << endl; // Print out the longest word.
+    
+    // Close the file and delete the list.
+    inputFile.close();
+    delete [] wordList;
+
+}
