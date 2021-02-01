37d68d7f83a05fabb0c508d98bafc39d333b4da5
Lucas Dahl <LucasDahl425@yahoo.com>
2021-02-01 13:48:41 -0800

    Added notes about vectors

--- CPP132/Notes/inheritance/InheratanceGradeExample.cpp
+++ CPP132/Notes/inheritance/InheratanceGradeExample.cpp
@@ -11,31 +11,31 @@
 
 using namespace std;
 
-int main() {
-    
-    GradedActivity a1(75);
-    GradedActivity a2(50);
-    
-    Quiz q1(4);
-    Homework h1(5, 8);
-  
-    vector<GradedActivity*> myScores;
-    
-    myScores.push_back(new GradedActivity(88));
-    myScores.push_back(new GradedActivity(32));
-    myScores.push_back(new Quiz(7));
-    myScores.push_back(new Homework(7,12));
-    myScores.push_back(new Homework(8,31));
-    myScores.push_back(new Quiz(8));
-    myScores.push_back(new ExtraCredit(77));
-    
-    for(int i = 0; i < myScores.size(); ++i) {
-        myScores[i]->printMe();
-    }
-    
-    // clean up
-    for(int i = 0; i < myScores.size(); ++i) {
-        delete myScores[i];
-    }
-    
-}
+//int main() {
+//    
+//    GradedActivity a1(75);
+//    GradedActivity a2(50);
+//    
+//    Quiz q1(4);
+//    Homework h1(5, 8);
+//  
+//    vector<GradedActivity*> myScores;
+//    
+//    myScores.push_back(new GradedActivity(88));
+//    myScores.push_back(new GradedActivity(32));
+//    myScores.push_back(new Quiz(7));
+//    myScores.push_back(new Homework(7,12));
+//    myScores.push_back(new Homework(8,31));
+//    myScores.push_back(new Quiz(8));
+//    myScores.push_back(new ExtraCredit(77));
+//    
+//    for(int i = 0; i < myScores.size(); ++i) {
+//        myScores[i]->printMe();
+//    }
+//    
+//    // clean up
+//    for(int i = 0; i < myScores.size(); ++i) {
+//        delete myScores[i];
+//    }
+//    
+//}
